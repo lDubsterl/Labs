613955513bebfa4b41bdd9563dc967dd8e8d20f9
Dubster <shevc9@mail.ru>
2023-11-04 01:00:04 +0300

    Added tasks for lab defence

--- Scala/ScalaVar2_lab4/Lab4/src/Main.scala
+++ Scala/ScalaVar2_lab4/Lab4/src/Main.scala
@@ -51,8 +51,38 @@ object Main7 {
     val text = "A mutable sequence of characters.\nThis class provides an API compatible with StringBuffer, but with no guarantee of synchronization.\nThis class is designed for use as a drop-in replacement for StringBuffer in places where the string buffer was being used by a single thread (as is generally the case).\nWhere possible, it is recommended that this class be used in preference to StringBuffer as it will be faster under most implementations. programming."
     val lines = text.split("\n")
     var filtered = lines.flatMap(line => line.split("\\W+"))
-    println(filtered.apply(filtered.size - 1))
+    println(filtered.apply(filtered.length - 1))
   }
 }
 
+
+object Def1 {
+  def main(args: Array[String]): Unit = {
+    var text = "A mutable sequence of characters.\nThis class provides an API compatible with StringBuffer, but with no guarantee of synchronization.\nThis class is designed for use as a drop-in replacement for StringBuffer in places where the string buffer was being used by a single thread (as is generally the case).\nWhere possible, it is recommended that this class be used in preference to StringBuffer as it will be faster under most implementations. programming."
+    //val lines = text.split("\n")
+    //var filtered = lines.flatMap(line => line.split("\\W+"))
+    var regex = "\\w{2}\\b".r
+    println(regex.replaceAllIn(text, "ing"))
+  }
+}
+
+object Def2 {
+  def main(args: Array[String]): Unit = {
+    var text = "A mutable sequence of characters.\nThis class provides an API compatible with StringBuffer, but with no guarantee of synchronization.\nThis class is designed for use as a drop-in replacement for StringBuffer in places where the string buffer was being used by a single thread (as is generally the case).\nWhere possible, it is recommended that this class be used in preference to StringBuffer as it will be faster under most implementations. programming."
+    var regex = "[aeiouyAEIOUY]".r
+    println(regex.replaceAllIn(text, ""))
+  }
+}
+
+object Def3 {
+  def main(args: Array[String]): Unit = {
+    var text = "A mutable sequence of characters." +
+      "\nThis class provides an API compatible with StringBuffer, but with no guarantee of synchronization." +
+      "\nThis class is designed for use as a drop-in replacement for StringBuffer in places where the string buffer" +
+      " was being used by a single thread (as is generally the case).\nWhere possible, it is recommended that this " +
+      "class be used in preference to StringBuffer as it will be faster under most implementations. programming. the"
+    val stopWords = "\\ba\\b|\\band\\b|\\bthe\\b|\\bof\\b".r
+    println(stopWords.replaceAllIn(text, ""))
+    }
+}
 // есть 2 предложения. сделать во всех словах ing окончания; удалить стоп слова; удалить все гласные;
